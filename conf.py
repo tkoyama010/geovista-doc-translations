49df8cebca6a448180f942999fb71ec1129fe3fc
Tetsuo Koyama <tkoyama010@gmail.com>
2024-07-03 04:47:46 +0900

    Add a project configuration file

---
+++ conf.py
@@ -0,0 +1,26 @@
+# basedir is set by <lang>/conf.py
+"""
+Use "-D language=<LANG>" option to build a localized paraview-docs document.
+For example::
+
+    sphinx-build -D language=ja -b html . _build/html
+
+This conf.py do:
+
+- Specify `locale_dirs`.
+- Overrides source directory as 'geovista/docs/src`.
+
+"""
+import os
+import pathlib
+
+basedir = os.path.join(
+    os.path.dirname(os.path.abspath(__file__)), "geovista/docs/src"
+)
+exec(pathlib.Path(os.path.join(basedir, "conf.py")).read_text(), globals())
+locale_dirs = [os.path.join(basedir, "../../../locale/")]
+
+
+def setup(app):
+    app.srcdir = basedir
+    app.confdir = app.srcdir
