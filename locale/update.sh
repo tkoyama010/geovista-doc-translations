aa069c192a18098fa08d4a3632ddb9b27cdd4af0
Tetsuo Koyama <tkoyama010@gmail.com>
2024-07-03 04:23:46 +0900

    Update update.sh

--- locale/update.sh
+++ locale/update.sh
@@ -11,7 +11,7 @@ set -ex
 # pull po files from transifex
 cd `dirname $0`
 #rm -R pot  # skip this line cause "already unused pot files will not removed" but we must keep these files to avoid commit for only "POT-Creation-Time" line updated. see: https://github.com/sphinx-doc/sphinx/issues/3443
-sphinx-build -T -D plot_gallery=0 -b gettext ../geovista/docs pot
+sphinx-build -T -D plot_gallery=0 -b gettext ../geovista/docs/src pot
 sphinx-intl update-txconfig-resources -p pot -d .
 cat .tx/config
 tx push -s --skip
