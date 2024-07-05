3b32c37771effd1ec0f7205ca4531c93320de6dc
Tetsuo Koyama <tkoyama010@gmail.com>
2024-07-06 04:45:50 +0900

    Used Ruff as a pre-commit hook via ruff-pre-commit (#12)
    * Used Ruff as a pre-commit hook via ruff-pre-commit
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update .pre-commit-config.yaml
    
    * Revert "[pre-commit.ci] auto fixes from pre-commit.com hooks"
    
    This reverts commit 80d24ce4f00103d74d171b953fdb785386fe28b9.
    
    * Update .pre-commit-config.yaml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update conf.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Create ruff.toml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update
    
    * Update conf.py
    
    * Update conf.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update conf.py
    
    * Update conf.py
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update conf.py
    
    * Update ruff.toml
    
    * Update ruff.toml
    
    * Update ruff.toml
    
    * Update ruff.toml
    
    * [pre-commit.ci] auto fixes from pre-commit.com hooks
    
    for more information, see https://pre-commit.ci
    
    * Update conf.py
    
    * Update conf.py
    
    * Update conf.py
    
    * Update conf.py
    
    ---------
    
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>

--- conf.py
+++ conf.py
@@ -1,6 +1,7 @@
 # basedir is set by <lang>/conf.py
 """
 Use "-D language=<LANG>" option to build a localized geovista-docs document.
+
 For example::
 
     sphinx-build -D language=ja -b html . _build/html
@@ -11,19 +12,17 @@ This conf.py do:
 - Overrides source directory as 'geovista/docs/src`.
 
 """
-import os
-import pathlib
 
-basedir = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)), "geovista/docs/src"
-)
-exec(pathlib.Path(os.path.join(basedir, "conf.py")).read_text(), globals())
-autoapi_dirs = [os.path.join(basedir, "../../../geovista/src/geovista/")]
-locale_dirs = [os.path.join(basedir, "../../../locale/")]
-package_dir = pathlib.Path(os.path.join(basedir, "../../../geovista/src/geovista/"))
-sphinx_gallery_conf["examples_dirs"] = str(package_dir / "examples")
+from pathlib import Path
+
+basedir = Path(__file__).resolve().parent / "geovista/docs/src"
+exec((basedir / "conf.py").read_text(), globals())  # noqa: S102
+autoapi_dirs = [basedir / "../../../geovista/src/geovista/"]
+locale_dirs = [basedir / "../../../locale/"]
+package_dir = basedir / "../../../geovista/src/geovista/"
+sphinx_gallery_conf["examples_dirs"] = package_dir / "examples"  # noqa: F821
 
 
-def setup(app):
+def setup(app) -> None:  # noqa: D103,ANN001
     app.srcdir = basedir
     app.confdir = app.srcdir
